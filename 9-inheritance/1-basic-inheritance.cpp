2bd2907818da33f3eb61dbc5e816453cc6124e34
= <sandhya.shiwakoti.dev@gmail.com>
2024-02-17 00:16:42 +0545

    Add inheritance files

---
+++ 9-inheritance/1-basic-inheritance.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+class Base {
+    public:
+        int x;
+        void display() { 
+            cout<<"Hello!";
+        }
+};
+class Derived : public Base {
+    public:
+        int y;
+        void show() {
+            cout<<"Bye";
+        }
+};
+
+int main() {
+    Base *p=new Derived();
+    p->display();
+    p->show();
+    return 0;
+}
